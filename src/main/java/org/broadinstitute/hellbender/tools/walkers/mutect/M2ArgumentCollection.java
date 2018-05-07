0c85670f40757ed1793c278eafaaed1b3d914bbc
takutosato <takuto.sato1@gmail.com>
2018-05-07 16:24:10 -0400

    New Orientation Bias Filter

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/M2ArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/M2ArgumentCollection.java
@@ -6,6 +6,8 @@ import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.hellbender.engine.FeatureInput;
 import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.AssemblyBasedCallerArgumentCollection;
 
+import java.io.File;
+
 public class M2ArgumentCollection extends AssemblyBasedCallerArgumentCollection {
     private static final long serialVersionUID = 9341L;
     public static final String TUMOR_SAMPLE_LONG_NAME = "tumor-sample";
@@ -32,7 +34,8 @@ public class M2ArgumentCollection extends AssemblyBasedCallerArgumentCollection 
     public static final String MAX_MNP_DISTANCE_LONG_NAME = "max-mnp-distance";
     public static final String MAX_MNP_DISTANCE_SHORT_NAME = "mnp-dist";
     public static final String IGNORE_ITR_ARTIFACTS_LONG_NAME = "ignore-itr-artifacts";
-
+    public static final String ARTIFACT_PRIOR_TABLE_LONG_NAME = "prior-artifact-probs";
+    public static final String ARTIFACT_PRIOR_TABLE_SHORT_NAME = "prior";
 
     public static final double DEFAULT_AF_FOR_TUMOR_ONLY_CALLING = 5e-8;
     public static final double DEFAULT_AF_FOR_TUMOR_NORMAL_CALLING = 1e-5;
@@ -135,6 +138,9 @@ public class M2ArgumentCollection extends AssemblyBasedCallerArgumentCollection 
      */
     @Argument(fullName = NORMAL_LOD_LONG_NAME, optional = true, doc = "LOD threshold for calling normal variant non-germline.")
     public double normalLod = 2.2;
+
+    @Argument(shortName = ARTIFACT_PRIOR_TABLE_SHORT_NAME, fullName = ARTIFACT_PRIOR_TABLE_LONG_NAME, optional = true, doc = "table of prior artifact probabilities for the read orientation filter model")
+    public File artifactPriorTable = null;
 
     /**
      * Two or more phased substitutions separated by this distance or less are merged into MNPs.
