2e3b2106d307e31dbf674e728193df7fad23c577
Samuel Friedman <sam@broadinstitute.org>
2018-07-20 16:42:51 -0400

    filter variant tranches removes old filters by default

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/FilterVariantTranchesIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/FilterVariantTranchesIntegrationTest.java
@@ -101,4 +101,26 @@ public class FilterVariantTranchesIntegrationTest  extends CommandLineProgramTes
         spec.executeTest("testTrancheFilteringTranchesOrder", this);
     }
 
+    @Test
+    public void testTrancheFilteringWithOldFilters() throws IOException {
+        final String trancheVCF = largeFileTestDir + "VQSR/g94982_20_1m_10m_python_2dcnn.vcf.gz";
+        final String indelTruthVCF = largeFileTestDir + "VQSR/ALL.wgs.indels_mills_devine_hg19_leftAligned_collapsed_double_hit.sites.20.1M-10M.vcf";
+        final String snpTruthVCF = largeFileTestDir + "VQSR/Omni25_sites_1525_samples.b37.20.1M-10M.vcf";
+
+        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
+        argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, trancheVCF)
+                .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s")
+                .addArgument("resource", snpTruthVCF)
+                .addArgument("resource", indelTruthVCF)
+                .addArgument("tranche", "99.0")
+                .addArgument("info-key", "MIX_SMALL_2D_W_DROPOUT")
+                .addArgument("keep-old-filters", "true")
+                .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
+
+        final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
+                Arrays.asList(largeFileTestDir + "VQSR/expected/g94982_20_1m_10m_tranched_99_with_old_filters.vcf"));
+        spec.executeTest("testTrancheFiltering", this);
+
+    }
+
 }
